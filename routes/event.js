5fd792cb88dd326cf2c0ce5a0b61b98fb4a30fbd
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-24 09:56:28 +0200

    Added Google Calendar to add event

--- routes/event.js
+++ routes/event.js
@@ -2,7 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const Event = require("../models/Event");
-const google = require("googleapis");
+let google = require("googleapis");
+google = google.google;
+const googleAuth = require('google-auth-library');
+
+var fs = require('fs');
+var OAuth2 = google.auth.OAuth2;
+//var googleAuth = require('google-auth-library');
+
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
+const googleSecrets = JSON.parse(fs.readFileSync('./config/client_secret.json')).installed;
+var oauth2Client = new OAuth2(
+  googleSecrets.client_id,
+  googleSecrets.client_secret,
+  googleSecrets.redirect_uris[0]
+);
+
+const token = fs.readFileSync(TOKEN_PATH);
+oauth2Client.setCredentials(JSON.parse(token));
+
+var calendar = google.calendar('v3');
 
 const googleMapsClient = require("@google/maps").createClient({
   key: process.env.MAPSAPI,
@@ -34,6 +56,7 @@ router.post("/new", (req, res, next) => {
       };
 
       const newEvent = new Event({ title, description, date, location });
+      saveCalendar(newEvent);
 
       newEvent
         .save()
@@ -42,6 +65,51 @@ router.post("/new", (req, res, next) => {
     })
     .catch(err => next(err));
 });
+
+function saveCalendar(event) {
+  googleMapsClient.reverseGeocode({latlng: [event.location.coordinates[0], event.location.coordinates[1]]}).asPromise()
+  .then(data => data.json.results[0].formatted_address)
+  .then(address => {
+    
+    const eventCalendar = {
+      'summary': event.title,
+      'location': address,
+      'description': event.description,
+      'start': {
+        'dateTime': event.date + 'T10:00:00+02:00',
+        'timeZone': 'Europe/Madrid',
+      },
+      'end': {
+        'dateTime': event.date + 'T12:00:00+02:00',
+        'timeZone': 'Europe/Madrid',
+      },
+      'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ],
+      'attendees': event.participants,
+      'reminders': {
+        'useDefault': false,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+    
+    calendar.events.insert({
+      auth: oauth2Client,
+      calendarId: 'primary',
+      resource: event,
+    }, function(err, event) {
+      if (err) {
+        console.log('There was an error contacting the Calendar service: ' + err);
+        return;
+      }
+      console.log('Event created');
+    });
+  })
+  .catch(err => console.log(err));  
+}
 
 router.get("/show/:id", (req, res, next) => {
   const id = req.params.id;
@@ -110,7 +178,7 @@ router.get("/delete/:id", (req, res, next) => {
   const id = req.params.id;
 
   Event.findByIdAndRemove(id)
-  .then( () => res.redirect("/event/list") )
+  .then( () => res.redirect("/event") )
   .catch( err => next(err) );
 })
 
