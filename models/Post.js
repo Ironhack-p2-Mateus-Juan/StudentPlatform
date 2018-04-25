f848df94381a4f4dd77e3c9f6f7553a792a29ec7
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-25 12:08:22 +0200

    Content is a String

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,7 @@ const postSchema = new Schema(
   {
     type: { type: String, default: "Post" },
     title: { type: String, required: true },
-    content: { type: Object, required: true },
+    content: { type: String, required: true },
     thumb: String,
     author: { type: Schema.Types.ObjectId, ref: "User" },
     imagePath: String
